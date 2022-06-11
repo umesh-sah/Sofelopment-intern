695ed77541ffea9fe836e47e59221745663572bb
umesh-sah <umesh.sah@sofelopment.com>
2022-06-11 18:55:26 +0545

    Solved the error of print function

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ Mathematical calculation of two numbers
 x = int(input("Enter the value of x:")) 
 y = int(input("enter the value of y:"))
 print(x+y) # Addition of two number
-# prit(x-y) # subtraction of two number
-print(x*y)
+print(x-y) # subtraction of two number
+print(x*y) # multiplication of two number
