331d082eaff156dd931cfb5f0215c1ac6e5ed876
Umesh Sah <107304087+umesh-sah@users.noreply.github.com>
2022-06-11 18:42:44 +0545

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+"""
+Author: 
+Date : 
+Mathematical calculation of two numbers
+"""
+
+x = int(input("Enter the value of x:")) 
+y = int(input("enter the value of y:"))
+print(x+y) # Addition of two number
+# prit(x-y) # subtraction of two number
+print(x*y)
